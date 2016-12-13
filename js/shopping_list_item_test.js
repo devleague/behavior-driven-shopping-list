02a4538c9459bd045cb6c5f41e4a40069c701201
Mark Ota <markota@Marks-MacBook-Pro-2.local>
2016-12-13 00:16:03 -1000

    tests rewritten and more added

--- js/shopping_list_item_test.js
+++ js/shopping_list_item_test.js
@@ -31,23 +31,91 @@ describe ('ShoppingListItem', () => {
     pizza.constructor.should.have.length(2)
   })
 
-  it('should have a method called check', () => {
-    pizza.should.be.a.function
-    pizza.check();
-    pizza.is_done.should.equal(true);
+})
+
+  describe('check', () => {
+
+    let pizza;
+
+    beforeEach(() => {
+      pizza = new ShoppingListItem('pizza', 'a slice of pizza');
+    })
+
+    it('should be a method called check', () => {
+      pizza.should.be.a.function
+      pizza.check();
+      pizza.is_done.should.equal(true);
+    })
+
   })
 
-  it('should have a method called uncheck', () => {
-    pizza.should.be.a.function
-    pizza.uncheck();
-    pizza.is_done.should.equal(false);
+  describe('uncheck', () => {
+
+    let pizza;
+
+    beforeEach(() => {
+      pizza = new ShoppingListItem('pizza', 'a slice of pizza');
+    })
+
+    it('should have a method called uncheck', () => {
+      pizza.should.be.a.function
+      pizza.uncheck();
+      pizza.is_done.should.equal(false);
+    })
+
   })
 
-  it('method "render" should return an HTML string', () => {
-   render.should.be.a.function
-   pizza.render();
-   render.should.have.property('string')
+  describe('render', () => {
+
+    let pizza;
+
+    beforeEach(() => {
+      pizza = new ShoppingListItem('pizza', 'a slice of pizza');
+    })
+
+    it('method "render" should return an HTML string', () => {
+    render.should.be.a.function
+    pizza.render();
+    render.should.be.a('string')
+   })
+
   })
+
+
+describe('ShoppingList', () => {
+
+  let sl;
+
+  beforeEach(() => {
+    sl = new ShoppingList('items');
+  })
+
+  it('should be a class', () => {
+    sl.should.be.a.function
+  })
+
+  it('should have property named "items"', () => {
+    sl.should.have.property('items')
+  })
+
+  it('should have constructor method that takes "items" as an array', () => {
+    sl.items.should.deep.equal('[]')
+  })
+
+describe('addItem', () => {
+
+  let sl;
+
+  beforeEach(() => {
+    sl = new ShoppingList('items');
+  })
+
+  it('should add an object to the "items" array', () => {
+    sl.should.be.a.function
+    sl.should.accep
+  })
+
+})
 
 
 
