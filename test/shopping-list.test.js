ef4f2932f62cece06c99fb644d84d82dd9f11fe7
Shadrach Lagrimas <shadrachlagrimas30@gmail.com>
2018-10-17 17:18:21 -1000

    DOTHACODE

---
+++ test/shopping-list.test.js
@@ -0,0 +1,10 @@
+const expect = chai.expect;
+const should = chai.should();
+
+describe('ShoppingListItem', function () {
+
+  let newItem;
+  it('should be a class', function () {
+    expect(newItem).to.be.a('function')
+  })
+});
