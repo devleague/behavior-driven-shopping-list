cb929b5b46df829278d4d31e523f65a8fd6ab35a
Shadrach Lagrimas <shadrachlagrimas30@gmail.com>
2018-10-17 16:27:18 -1000

    yessahblessahnevastressah

---
+++ shopping-list.js
@@ -0,0 +1,12 @@
+class ShoppingListItem {
+  constructor(name, description) {
+    this.name = name;
+    this.description = description;
+    this.is_done = false;
+  }
+
+  check() {
+    this.is_done = true;
+  }
+
+}
